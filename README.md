585eceb4a20f5e090bd0eacbb17b746fecb90c05
Valter Kraemer <valter.kraemer@aalto.fi>
2017-08-23 17:48:39 +0300

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# Mavo-firebase
+
+A Firebase backend plugin for [Mavo](https://mavo.io).
+
+## Setup mavo-firebase
+
+Add Firebase and initialize it in the `<head>` of your HTML file.
+
+    <script src="https://www.gstatic.com/firebasejs/4.3.0/firebase.js"></script>
+    <script src="path/to/mavo-firebase.js"></script>
+    <script>
+      firebase.initializeApp({
+        apiKey: '...',
+        authDomain: '...',
+        databaseURL: '...',
+        projectId: '...',
+        storageBucket: '...',
+        messagingSenderId: '...'
+      });
+    </script>
+
+Check out the [Setup Firebase](#setup-firebase) section to find out how to set up a Firebase database.
+
+## Setup Firebase
+
+Login or signup at https://console.firebase.google.com
+
+### Create a new project
+
+![1-add-project](assets/images/1-add-project.png "Add project")
+
+![2-create-project](assets/images/2-create-project.png "Create project")
+
+### Get your project's config details
+
+Insert these values into the `firebase.initializeApp` in your app.
+
+![3-add-firebase-to-your-web-app](assets/images/3-add-firebase-to-your-web-app.png "Add firebase to your web app")
+
+![4-view-config](assets/images/4-view-config.png "View config")
+
+### Setup database rules
+
+To be able to read and write to the database we will make it public.
+
+**Warning:** After this step anyone can read or write to your database.
+
+![5-go-to-database](assets/images/5-go-to-database.png "Go to database")
+
+![6-go-to-rules](assets/images/6-go-to-rules.png "Go to rules")
+
+Set `.read` and `.write` to `true`. Remember to click "Publish".
+
+![7-edit-rules](assets/images/7-edit-rules.png "Edit rules")
---
+++ src/mavo-firebase.js
@@ -0,0 +1,163 @@
+(function() {
+  var $ = window.Bliss;
+  var Mavo = window.Mavo;
+
+  Mavo.Backend.register($.Class({
+    extends: Mavo.Backend,
+    id: 'Firebase',
+    constructor: function() {
+      this.key = this.mavo.id;
+      this.db = firebase.database().ref('mavo');
+
+      this.permissions.on(['read', 'edit', 'add', 'delete', 'save', 'login']);
+      this.storeIds = new Set();
+
+      // Localstorage changes
+      window.addEventListener('storage', e => {
+        if (e.key !== this.key) {
+          return;
+        }
+
+        var newValue = parseJSON(e.newValue);
+        this.updateView(newValue, true);
+      });
+
+      // Firebase changes
+      this.db.on('value', (snapshot) => {
+        this.updateView(snapshot.val());
+      });
+
+      // Firebase auth changes
+      firebase.auth().onAuthStateChanged(user => {
+        if (!user) {
+          return;
+        }
+
+        this.permissions.on(['read', 'edit', 'add', 'delete', 'save', 'logout']);
+        this.user = {
+          username: user.email,
+          name: user.displayName,
+          avatar: user.photoURL
+        };
+
+      }, error => {
+        this.mavo.error('Firebase: ' + error.message);
+      });
+
+    },
+
+    load: function() {
+      var newValue = parseJSON(localStorage[this.key]);
+
+      if (newValue && newValue.storeId) {
+        this.storeIds.add(newValue.storeId);
+
+        // Send to server
+        this.put(newValue);
+      }
+
+      return Promise.resolve(newValue);
+    },
+
+    store: function(data) {
+      // Prevent render to fire store
+      if (!this.mavo.unsavedChanges) {
+        return Promise.resolve();
+      }
+
+      var storeId = Date.now() + '-' + Math.floor((Math.random() * 100000));
+      this.storeIds.add(storeId);
+      data.storeId = storeId;
+
+      // Put to localstorage
+      this.putLocalStorage(data);
+
+      return this.put(data)
+        .catch((err) => this.mavo.error('Firebase: ' + err.message));
+    },
+
+    put: function(data) {
+      if (!data || !data.storeId) {
+        return Promise.reject(new Error('No put data or storeId'));
+      }
+
+      return this.db.transaction((currentData) => {
+        if (!currentData || !currentData.storeId) {
+          return data;
+        }
+
+        // Ignore if it's the same item (can happen when gotten from localStorage)
+        if (data.storeId === currentData.storeId) {
+          return;
+        }
+
+        if (this.storeIds.has(currentData.storeId)) {
+          return data;
+        }
+
+        if (confirm('Overwrite server with local data')) {
+          return data;
+        }
+
+        this.updateView(currentData);
+        return;
+      });
+    },
+
+    putLocalStorage: function(data) {
+      localStorage[this.key] = JSON.stringify(data);
+    },
+
+    login: function() {
+      return this.ready.then(() => {
+        if (this.user) {
+          return;
+        }
+
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        return firebase.auth().signInWithPopup(provider).catch(error => {
+          this.mavo.error('Firebase: ' + error.message);
+        });
+      });
+    },
+
+    logout: function() {
+      firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        this.mavo.error('Firebase: ' + error.message);
+      });
+    },
+
+    updateView: function(data, ignoreLocalStorage) {
+      if (data && data.storeId) {
+        if (this.storeIds.has(data.storeId)) {
+          return;
+        }
+
+        this.storeIds.add(data.storeId);
+      }
+
+      if (!ignoreLocalStorage) {
+        this.putLocalStorage(data);
+      }
+
+      this.mavo.render(data);
+      this.mavo.setUnsavedChanges(false);
+    },
+
+    static: {
+      test: value => value == 'firebase'
+    }
+  }));
+
+  function parseJSON(obj) {
+    try {
+      return JSON.parse(obj);
+    } finally {
+      // Avoid no-empty lint error
+    }
+  }
+
+})();
