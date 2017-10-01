598f009e65a9183d05bb32bb37cb94086665d5b9
Valter Kraemer <valter.kraemer@aalto.fi>
2017-10-01 19:05:20 +0300

    Big refactor to support offline-storage component

--- src/mavo-firebase.js
+++ src/mavo-firebase.js
@@ -1,193 +1,179 @@
-(function() {
-  var $ = window.Bliss;
-  var Mavo = window.Mavo;
+(function () {
+  const $ = window.Bliss
+  const Mavo = window.Mavo
+  const firebase = window.firebase
 
   Mavo.Backend.register($.Class({
     extends: Mavo.Backend,
     id: 'Firebase',
-    constructor: function() {
-      this.key = this.mavo.id;
-      this.db = firebase.database().ref('mavo');
+    constructor: function () {
+      this.key = this.mavo.id
+      this.db = firebase.database().ref('mavo')
+
+      this.statusChangesCallbacks = []
+      this.changesCallbacks = []
 
       this.fbPermissions = {
         default: ['read', 'login'],
         authenticated: ['read', 'edit', 'add', 'delete', 'save', 'logout']
-      };
+      }
 
-      var defaultPermissions = this.mavo.element.getAttribute('firebase-default-permissions');
+      let defaultPermissions = this.mavo.element.getAttribute('firebase-default-permissions')
       if (defaultPermissions) {
-        this.fbPermissions.default = defaultPermissions.split(/\s+/);
+        this.fbPermissions.default = defaultPermissions.split(/\s+/)
       } else if (defaultPermissions === '') {
-        this.fbPermissions.default = [];
+        this.fbPermissions.default = []
       }
 
-      var authPermissions = this.mavo.element.getAttribute('firebase-authenticated-permissions');
+      let authPermissions = this.mavo.element.getAttribute('firebase-authenticated-permissions')
       if (authPermissions) {
-        this.fbPermissions.authenticated = authPermissions.split(/\s+/);
+        this.fbPermissions.authenticated = authPermissions.split(/\s+/)
       } else if (authPermissions === '') {
-        this.fbPermissions.authenticated = [];
+        this.fbPermissions.authenticated = []
       }
 
-      this.permissions.on(this.fbPermissions.default);
-      this.storeIds = new Set();
-
-      // Localstorage changes
-      window.addEventListener('storage', e => {
-        if (e.key !== this.key) {
-          return;
-        }
-
-        var newValue = parseJSON(e.newValue);
-        this.updateView(newValue, true);
-      });
-
-      // Firebase changes
-      this.db.on('value', (snapshot) => {
-        this.updateView(snapshot.val());
-      });
+      this.permissions.on(this.fbPermissions.default)
 
       // Firebase auth changes
       firebase.auth().onAuthStateChanged(user => {
         if (!user) {
-          return;
+          return
         }
 
-        this.permissions.off(this.fbPermissions.default).on(this.fbPermissions.authenticated);
+        this.permissions.off(this.fbPermissions.default).on(this.fbPermissions.authenticated)
         this.user = {
           username: user.email,
           name: user.displayName,
           avatar: user.photoURL
-        };
-
+        }
       }, error => {
-        this.mavo.error('Firebase: ' + error.message);
-      });
+        this.mavo.error('Firebase: ' + error.message)
+      })
     },
 
-    load: function() {
-      var newValue = parseJSON(localStorage[this.key]);
+    onStatusChange: function (callback) {
+      this.statusChangesCallbacks.push(callback)
 
-      if (newValue && newValue.storeId) {
-        this.storeIds.add(newValue.storeId);
-
-        // Send to server
-        this.put(newValue);
+      if (this.listeningOnStatus) {
+        return
       }
+      this.listeningOnStatus = true
 
-      return Promise.resolve(newValue);
+      firebase.database().ref('.info/connected').on('value', snap => {
+        this.statusChangesCallbacks.forEach(callback => callback(snap.val()))
+      })
     },
 
-    store: function(data) {
-      // Prevent render to fire store
-      if (!this.mavo.unsavedChanges) {
-        return Promise.resolve();
+    onChange: function (callback) {
+      this.changesCallbacks.push(callback)
+
+      if (this.listeningOnValue) {
+        return
       }
+      this.listeningOnValue = true
 
-      var storeId = Date.now() + '-' + Math.floor((Math.random() * 100000));
-      this.storeIds.add(storeId);
-      data.storeId = storeId;
+      console.log('onChange')
 
-      // Put to localstorage
-      this.putLocalStorage(data);
+      this.db.on('value', snapshot => {
+        let doc = snapshot.val()
 
-      return this.put(data)
-        .catch((err) => this.mavo.error('Firebase: ' + err.message));
+        console.log('doc', doc)
+
+        if (!doc || !doc._rev || doc._rev <= this.rev) {
+          return
+        }
+
+        this.rev = doc._rev
+
+        this.changesCallbacks.forEach(callback => callback(doc))
+      })
     },
 
-    put: function(data) {
-      if (!data || !data.storeId) {
-        return Promise.reject(new Error('No put data or storeId'));
-      }
+    load: function () {
+      return this.db.once('value').then(snapshot => {
+        let data = snapshot.val()
 
-      return this.db.transaction((currentData) => {
-        if (!currentData || !currentData.storeId) {
-          return data;
+        if (!data) {
+          this.rev = 0
+          return {}
         }
 
-        // Ignore if it's the same item (can happen when gotten from localStorage)
-        if (data.storeId === currentData.storeId) {
-          return;
-        }
-
-        if (this.storeIds.has(currentData.storeId)) {
-          return data;
-        }
-
-        if (confirm('Overwrite server with local data')) {
-          return data;
-        }
-
-        this.updateView(currentData);
-        return;
-      });
+        this.rev = data._rev
+        return data
+      })
     },
 
-    putLocalStorage: function(data) {
-      localStorage[this.key] = JSON.stringify(data);
+    store: function (data) {
+      // TODO: How about conflicts?
+
+      return this.db.transaction(currentData => {
+        let newRev = 0
+
+        if (currentData && currentData._rev) {
+          newRev = currentData._rev + 1
+        }
+
+        this.rev = newRev
+
+        return Object.assign(data, {
+          _rev: newRev
+        })
+      })
     },
 
-    login: function() {
+    login: function () {
       return this.ready.then(() => {
         if (this.user) {
-          return;
+          return
         }
 
-        var provider = new firebase.auth.GoogleAuthProvider();
+        let provider = new firebase.auth.GoogleAuthProvider()
 
         return firebase.auth().signInWithPopup(provider).catch(error => {
-          this.mavo.error('Firebase: ' + error.message);
-        });
-      });
+          this.mavo.error('Firebase: ' + error.message)
+        })
+      })
     },
 
-    logout: function() {
-      firebase.auth().signOut().then(() => {
+    logout: function () {
+      return firebase.auth().signOut().then(() => {
         // Sign-out successful.
-        this.permissions.off(this.fbPermissions.authenticated).on(this.fbPermissions.default);
-
+        this.permissions.off(this.fbPermissions.authenticated).on(this.fbPermissions.default)
       }).catch((error) => {
-        this.mavo.error('Firebase: ' + error.message);
-      });
+        this.mavo.error('Firebase: ' + error.message)
+      })
     },
 
-    updateView: function(data, ignoreLocalStorage) {
-      if (data && data.storeId) {
-        if (this.storeIds.has(data.storeId)) {
-          return;
-        }
+    upload: function (file) {
+      this.storage = this.storage || firebase.storage().ref('mavo')
 
-        this.storeIds.add(data.storeId);
-      }
-
-      if (!ignoreLocalStorage) {
-        this.putLocalStorage(data);
-      }
-
-      this.mavo.render(data);
-      this.mavo.setUnsavedChanges(false);
-    },
-
-    upload: function(file) {
-      this.storage = this.storage || firebase.storage().ref('mavo');
-
-      var ref = this.storage.child(`${file.name}-${Date.now()}`);
+      let ref = this.storage.child(`${file.name}-${Date.now()}`)
 
       return ref.put(file).then(() => {
-        return ref.getDownloadURL();
-      });
+        return ref.getDownloadURL()
+      })
+    },
+
+    compareDocRevs: function (docA, docB) {
+      // If b is newer return 1
+
+      if (!docA || !docA._rev) {
+        return 1
+      }
+
+      if (!docB || !docB._rev) {
+        return -1
+      }
+
+      if (docA._rev === docB._rev) {
+        return 0
+      }
+
+      return docA._rev < docB._rev ? 1 : -1
     },
 
     static: {
-      test: value => value == 'firebase'
+      test: value => value === 'firebase'
     }
-  }));
-
-  function parseJSON(obj) {
-    try {
-      return JSON.parse(obj);
-    } catch(e) {
-      // Avoid no-empty lint error
-    }
-  }
-
-})();
+  }))
+})()
